1c1f980d7ad00647558f2881bdb51593efe8bd81
Maurice <maurice@Maurices-iMac.local>
2026-08-18 11:52:12 -0700

    Allow shared GoodOS interface styles

--- worker/index.ts
+++ worker/index.ts
@@ -60,7 +60,7 @@ const worker = {
     );
     headers.set(
       "Content-Security-Policy",
-      "default-src 'self'; base-uri 'self'; object-src 'none'; frame-ancestors 'none'; form-action 'self' https://base.goodos.app; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https:; connect-src 'self' https://base.goodos.app https://api.stripe.com https://api-m.stripe.com https://api-m.sandbox.paypal.com https://api-m.paypal.com; upgrade-insecure-requests",
+      "default-src 'self'; base-uri 'self'; object-src 'none'; frame-ancestors 'none'; form-action 'self' https://base.goodos.app; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://base.goodos.app; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: blob: https:; connect-src 'self' https://base.goodos.app https://api.stripe.com https://api-m.stripe.com https://api-m.sandbox.paypal.com https://api-m.paypal.com; upgrade-insecure-requests",
     );
     headers.set(
       "Strict-Transport-Security",
